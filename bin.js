a2f0f7bc121d72d09554a13ab79b5ccf7acd8ee6
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-01-25 01:44:46 +0100

    Implement JSON cache file

--- bin.js
+++ bin.js
@@ -19,21 +19,33 @@ else {
   process.exit(1)
 }
 
+/**
+ * Get cached metadata from file
+ */
+let meta
+
+try {
+  const cache = fs.readFileSync(getDefaultSlug() + '.json')
+  meta = JSON.parse(cache) || {}
+}
+catch (_) {
+  meta = {authors: []}
+}
 
 /**
- * Get metadata through prompt
+ * Get updated metadata through prompt
  */
 const fields = [
   {name: 'couch', type: 'string', default: 'http://localhost:5984', pattern: /^http/, message: 'Target must be a valid CouchDB url'},
-  {name: 'site', type: 'string', required: true, message: 'Please specify the site to publish to'},
+  {name: 'site', type: 'string', default: meta.site, required: true, message: 'Please specify the site to publish to'},
+  {name: 'type', type: 'string', default: meta.type, required: true, message: 'Please select a type for your entry (for example: page, post, etc.)'},
+  {name: 'slug', type: 'string', default: meta.slug || getDefaultSlug()},
+  {name: 'title', type: 'string', default: meta.title, required: true, message: 'Entry title is required (see: https://en.wikipedia.org/wiki/RSS#RSS_compared_to_Atom)'},
+  {name: 'author', type: 'string', default: meta.authors[0], required: true, message: 'Entry author is required (see: https://en.wikipedia.org/wiki/RSS#RSS_compared_to_Atom)'},
+  {name: 'subtitle', type: 'string', default: meta.subtitle},
+  {name: 'summary', type: 'string', default: meta.summary},
   {name: 'username', type: 'string'},
-  {name: 'password', type: 'string', hidden: true},
-  {name: 'type', type: 'string', required: true, message: 'Please select a type for your entry (for example: page, post, etc.)'},
-  {name: 'slug', type: 'string', default: getDefaultSlug()},
-  {name: 'title', type: 'string', required: true, message: 'Entry title is required (see: https://en.wikipedia.org/wiki/RSS#RSS_compared_to_Atom)'},
-  {name: 'author', type: 'string', required: true, message: 'Entry author is required (see: https://en.wikipedia.org/wiki/RSS#RSS_compared_to_Atom)'},
-  {name: 'subtitle', type: 'string'},
-  {name: 'summary', type: 'string'}
+  {name: 'password', type: 'string', hidden: true}
 ]
 
 prompt.message = 'couch.pm'
@@ -49,27 +61,43 @@ prompt.get(fields, function(err, result) {
     throw new Error(err)
   }
 
+  // Connect CouchDB and authenticate
   const url = `${result.couch}/${result.site}`
   const couch = new PouchDB(url, {auth: {
     username: result.username,
     password: result.password
   }})
 
-  result._id = result.slug
+  delete result.couch
+  delete result.username
+  delete result.password
+
+  // Put author in array (future-proof)
   result.authors = [result.author]
+  delete result.author
+
+  // Cache metadata to JSON file
+  const json = JSON.stringify(result, null, 2)
+  fs.writeFile(getDefaultSlug() + '.json', json, 'UTF-8', err => console.error)
+
+  delete result.site
+
+  // Prepare documet for CouchDB
+  result._id = result.slug
   result.content = content
   result.updated = now()
 
-  delete result.couch
-  delete result.site
-  delete result.username
-  delete result.password
   delete result.slug
-  delete result.author
 
+  // Make `created` timestamp,
+  // will be overwritten if
+  // document exists
+  const stamp = {created: result.updated}
+
+  // Write document to CouchDB
   couch.get(result._id).
     catch(_ => result).
-    then(doc => xtend(doc, result)).
+    then(doc => xtend(stamp, doc, result)).
     then(entry => couch.put(entry)).
     then(_ => console.info('couch.pm -> posted!')).
     catch(console.error)
